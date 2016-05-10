e24ca06ceb850f758542b64369c3e8bd732623cb
nuronial_block <ahnaf@furqansoftware.com>
2016-05-11 03:21:49 +0600

    Implement class example

--- code0.py
+++ code0.py
@@ -8,4 +8,4 @@ features = [[140, 1], [130, 1], [150, 0], [170, 0]]
 labels = [0, 0, 1, 1]
 clf = tree.DecisionTreeClassifier() # Rules
 clf = clf.fit(features,labels) # Algorithm to find patterns in data
-print clf.predict([[150,0]])
+print clf.predict([[145,0]])
